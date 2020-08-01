241d8454a28be679b38d6e26f6baf6ac31bbbbaf
ritanshoo <ritanshukumarsingh8922@gmail.com>
2020-08-01 13:43:21 +0530

    just some basics stuffs

---
+++ 01_NodeJs_Basics/app.js
@@ -0,0 +1,22 @@
+//simple log message
+console.log('Ritanshu welcome to NodeJs');
+
+//object
+let student={
+    name:'Ritanshu',
+    age:18,
+    course:'computer science',
+    address:'chhapra'
+};
+console.log(student);
+
+// array
+let hobby = ['chess','cricket','coding','exploring','web-dev'];
+
+console.log(hobby);
+
+//function
+let greetMe=(name)=>{
+    console.log(`Hello ${name} Good afternoon`);
+};
+greetMe('Ritanshu');
